7f97fc96445fe81953ee22d2b0fd45e921750799
sirUttam <blimshorts@gmail.com>
2026-06-21 00:00:18 +0545

    minor changes

--- app/routes/navbar.py
+++ app/routes/navbar.py
@@ -3,29 +3,31 @@ from app.database import get_db
 from app.auth.dependencies import get_current_user
 from sqlalchemy.orm import Session
 from app.models.navbar import SiteSettings
-from app.schemas.navbar import NavbarUpdate
+from app.schemas.navbar import NavbarUpdate, NavbarResponse
 
 
 router = APIRouter()
 
 
 
-@router.get("/settings/navbar")
+@router.get("/settings/navbar", response_model=NavbarResponse)
 def get_navbar(db: Session = Depends(get_db)):
     return db.query(SiteSettings).first()
 
 
 
-@router.put("/settings/navbar")
-def update_navbar(data: NavbarUpdate, db: Session = Depends(get_db)):
+@router.put("/settings/navbar", response_model=NavbarResponse)
+def update_navbar(data: NavbarUpdate, current_user: dict = Depends(get_current_user), db: Session = Depends(get_db)):
 
     settings = db.query(SiteSettings).first()
 
     if not settings:
         settings = SiteSettings()
 
-    settings.logo_url = data.logo_url
-    settings.site_name = data.site_name
+    updated_data = data.model_dump(exclude_unset=True)
+
+    for key, value in updated_data.items():
+        setattr(settings, key, value)
 
     db.add(settings)
     db.commit()
--- app/schemas/navbar.py
+++ app/schemas/navbar.py
@@ -5,3 +5,13 @@ from pydantic import BaseModel
 class NavbarUpdate(BaseModel):
     logo_url: str | None = None
     site_name: str | None = None
+    
+    
+class NavbarResponse(BaseModel):
+    id: int
+    logo_url: str | None = None
+    site_name: str
+
+    model_config = {
+        "from_attributes": True
+    }
