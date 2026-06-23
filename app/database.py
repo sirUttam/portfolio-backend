c1cf2448bbc12be2ed26a2c3973dbe31019d830e
sirUttam <blimshorts@gmail.com>
2026-06-23 14:51:37 +0545

    migrate to postgres

--- app/database.py
+++ app/database.py
@@ -7,20 +7,22 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# safety fix for Render Postgres
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-SessionLocal = sessionmaker(bind=engine)
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
 
 
-# creating dependency Injection for database session
 
+# dependency injection
 def get_db():
-    
     db = SessionLocal()
-
     try:
         yield db
     finally:
